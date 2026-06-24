7d82e44768eb6ae348cf3739e05c1110498e14aa
dragon15290662499 <15290662499@163.com>
2026-06-24 10:02:40 +0800

    测试

---
+++ Assets/Scripts/Hellow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hellow : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Hellow Unity");
+    }
+}
